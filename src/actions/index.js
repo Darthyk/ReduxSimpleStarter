0783e2bd8dd8f3aed70eebcf330ce6d894c2c54c
Vladislav Sazhin <vsazhin@wiley.com>
2018-05-30 16:57:07 +0300

    middle update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '1f4ca8a0845ac0f72a6a89e872168d25';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    console.log('Request:', request);
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
